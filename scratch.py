6b362ba443f9da269e41c7e953f5973a324dea42
muskoxcommune <muskoxcommune@gmail.com>
2021-05-30 23:27:20 -0700

    Initial commit

---
+++ scratch.py
@@ -0,0 +1,133 @@
+#!/opt/homebrew/bin/python3
+from ratelimit import limits  # https://pypi.org/project/ratelimit
+
+import argparse
+import json
+import logging
+import pathlib
+import platform
+import pprint
+import requests
+import time
+import traceback
+import uuid
+
+access_token_filename = '/tmp/reddit.token.json'
+access_token_path = pathlib.Path(access_token_filename)
+access_token = None
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--id', required=True, help='app client ID')
+argparser.add_argument('--password', required=True, help='reddit development user password')
+argparser.add_argument('--secret', required=True, help='app client secret')
+argparser.add_argument('--user', required=True, help='reddit development user')
+args = argparser.parse_args()
+
+age_cutoff_seconds = 3600 * 24
+stats = {
+    'script': {
+        'status_codes': {}},
+    'stock': {}}
+subreddit_list = ['gme']
+subreddit_to_symbol_map = {
+    'gme': 'GME'
+}
+user_agent = (platform.system() + ' ' + platform.release() + '; '
+              + 'reddit_collector/v0.1 (by /u/' + args.user + ')')
+
+logging.basicConfig(
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    encoding='utf-8',
+    format='%(levelname)s\t%(asctime)s\t%(message)s',
+    level=logging.DEBUG)
+
+# Fetch new token if token file is not found or token file is older
+# than 59 minutes. Default token expiration is 60 minutes.
+if (not access_token_path.exists()
+        or float(time.time()) - access_token_path.stat().st_mtime > 3540):
+    logging.info('Fetching new access token')
+    response = requests.post(
+        url='https://www.reddit.com/api/v1/access_token',
+        auth=requests.auth.HTTPBasicAuth(args.id, args.secret),
+        data={
+            'grant_type': 'password',
+            'username': args.user,
+            'password': args.password
+        },
+        headers={
+            'User-Agent': user_agent,
+        })
+    if response.status_code == 200:
+        access_token = response.json()['access_token']
+        with open(access_token_filename, 'w') as fd:
+            fd.write(response.content.decode('utf-8'))
+else:
+    logging.info('Using existing access token')
+    with open(access_token_filename) as fd:
+        access_token = json.load(fd)['access_token']
+
+logging.info('Using token %s, last modified %d seconds ago',
+    access_token, float(time.time()) - access_token_path.stat().st_mtime)
+
+@limits(calls=1, period=1)
+def do_get(api_url, api_params={}):
+    response = requests.get(
+        url=api_url,
+        params=api_params,
+        headers={
+            'Authorization': 'bearer ' + access_token,
+            'User-Agent': user_agent,
+        })
+    logging.debug('%s %s', response, response.headers)
+    if response.status_code in stats['script']['status_codes']:
+        stats['script']['status_codes'][response.status_code] += 1
+    else:
+        stats['script']['status_codes'][response.status_code] = 1
+    return response
+
+def process_article_list(response, subreddit_name=None):
+    response_data = response.json()
+    try:
+        assert response_data['data']
+        assert response_data['data']['children']
+        logging.debug('Processing %s articles', len(response_data['data']['children']))
+        age = 0
+        for article in response_data['data']['children']:
+            age = float(time.time()) - float(article['data']['created_utc'])
+            if age >= age_cutoff_seconds:
+                break
+            if not article['data']['selftext']:
+                logging.debug('created_utc: %s, age: %s, ups: %s, num_comments: %s, num_crossposts: %s, type: url, title: %s',
+                    article['data']['created_utc'],
+                    age,
+                    article['data']['ups'],
+                    article['data']['num_comments'],
+                    article['data']['num_crossposts'],
+                    article['data']['title'])
+            else:
+                logging.debug('created_utc: %s, age: %s, ups: %s, num_comments: %s, num_crossposts: %s, type: selftext, title: %s',
+                    article['data']['created_utc'],
+                    age,
+                    article['data']['ups'],
+                    article['data']['num_comments'],
+                    article['data']['num_crossposts'],
+                    article['data']['title'])
+        return None if age > age_cutoff_seconds else response_data['data']['after']
+    except Exception as exc:
+        logging.error(traceback.format_exc())
+
+for subreddit_name in subreddit_list:
+    after = None
+    api_url = 'https://oauth.reddit.com/r/' + subreddit_name + '/new'
+    paginate_enabled = True
+    while paginate_enabled is True:
+        api_params={'limit': 100, 'show': 'all'}
+        if after is not None:
+            api_params['after'] = after
+        response = do_get(api_url, api_params=api_params)
+        if response.status_code == 200:
+            after = process_article_list(response, subreddit_name=subreddit_name)
+            if after is None:
+                paginate_enabled = False
+logging.info('Script stats: %s', stats['script'])
+logging.info('Stock stats: %s', stats['stock'])
